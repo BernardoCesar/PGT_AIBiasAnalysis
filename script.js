e00d7240c59aa95b67c875147127025ae1cd9dbe
Diego Calciolari Gordiano <diego.gordiano@acad.espm.br>
2024-05-14 23:25:19 -0300

    Feat: axios and remove base64

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const prompts = require('./prompts')
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
 const url_api = 'http://127.0.0.1:7860';
 
 //url da api de consulta de imagens https://labs.everypixel.com/demo
@@ -15,46 +16,45 @@ fs.stat(out_dir_t2i, exist => {
 });
 
 function decode_and_save_base64(base64_str, save_path) {
-  fs.writeFileSync(save_path, Buffer.from(base64.decode(base64_str)));
+  const buffer = Buffer.from(base64_str, 'base64');
+  fs.writeFileSync(save_path, buffer); 
 }
 
 async function call_api(api_endpoint, payload) {
   const data = JSON.stringify(payload);
-  const request = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: data
-  };
-  
+
   try {
-    const response = await fetch(`${url_api}/${api_endpoint}`, request);
-    return await response.json();
+    const response = await axios ({  
+        method: 'post', 
+        url: `${url_api}/${api_endpoint}`, 
+        headers: {'Content-Type': 'application/json'}, 
+        data: data
+    })
+    return response.data;
   } catch (error) {
-    return console.error('Error:', error);
-  }
+    console.error('Erro ao chamar a API:', error.message);
+    throw error; 
+  }
 }
 
 async function call_txt2img_api (payload, prompt, numero) {
   await call_api('sdapi/v1/txt2img', payload)
     .then(response => {
       if (response && response.images) {
-        response.images.forEach((image) => {
+        response.images.forEach(async (image) => {
           const save_path = path.join(out_dir_t2i, `txt2img-${prompt}-${numero}.png`);
-          decode_and_save_base64(image, save_path);
+          await decode_and_save_base64(image, save_path);
         });
       }
     });
 }
 
-let seed = 0;
-for(let aux = 0; aux<prompts.length; aux++){
-  for(let i = 0; i<10; i++){
-    const payload ={
-      "prompt": prompts[aux],
+for(let aux = 1; aux < prompts.length; aux++){
+  for(let i = 1; i < 3; i++){
+    const payload = {
+      "prompt": prompts[aux-1],
       "negative_prompt": "drawing, painting, crayon, sketch, graphite, impressionist, noisy, blurry, soft, deformed",
-      "seed": seed,
+      "seed": i,
       "steps": 25,
       "width": 896,
       "height": 1152,
@@ -63,7 +63,6 @@ for(let aux = 0; aux<prompts.length; aux++){
       "n_iter": 1,
       "batch_size": 3,
     }
-    //call_txt2img_api(payload, aux, i);
-    seed++;
+    call_txt2img_api(payload, aux, i);
   }
 }
